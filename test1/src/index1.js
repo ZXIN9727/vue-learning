fead673ee1287740e2f760eb8ea37c0d5b900543
zxin <zhangxin9727@foxmail.com>
2022-07-24 23:00:00 +0800

    2022/7/24 22:57

---
+++ test1/src/index1.js
@@ -0,0 +1,20 @@
+import $ from 'jquery'
+import './css/index.css'
+import './css/index.less'
+import logo from './images/logo.jpeg'
+
+$('.box').attr('src', logo)
+
+$(function () {
+    $('li:odd').css('background-color', 'red')
+    $('li:even').css('background-color', 'pink')
+})
+
+function info(target){
+    target.info='Person info.'
+}
+
+@info
+class Person{}
+
+console.log(Person.info)
---
+++ test1/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path")
+const HtmlPlugin = require('html-webpack-plugin')
+const htmlPlugin = new HtmlPlugin({
+    template: './src/index.html', filename: './index.html'
+})
+
+module.exports = {
+    mode: 'production', entry: path.join(__dirname, './src/index1.js'), output: {
+        path: path.join(__dirname, './dist/'), filename: 'bundle.js'
+    }, plugins: [htmlPlugin], devServer: {
+        open: true, port: 80, host: '127.0.0.1'
+    }, module: {
+        rules: [{
+            test: /\.css$/, use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']
+        }, {
+            test: /\.jpeg|jpg|gif|png$/, use: 'url-loader?limit=1024'
+        }, {
+            test: /\.js/, use: 'babel-loader', exclude: '/node_modules'
+        }]
+    }
+}
