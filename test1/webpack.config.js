d9eed58ea2672151ae591d0525b61948fd808f40
zxin <zhangxin9727@foxmail.com>
2022-07-26 00:23:54 +0800

    2022/7/26 0:23

--- test1/webpack.config.js
+++ test1/webpack.config.js
@@ -3,11 +3,12 @@ const HtmlPlugin = require('html-webpack-plugin')
 const htmlPlugin = new HtmlPlugin({
     template: './src/index.html', filename: './index.html'
 })
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
-    mode: 'production', entry: path.join(__dirname, './src/index1.js'), output: {
-        path: path.join(__dirname, './dist/'), filename: 'bundle.js'
-    }, plugins: [htmlPlugin], devServer: {
+    mode: 'development', devtool: 'nosources-source-map', entry: path.join(__dirname, './src/index1.js'), output: {
+        path: path.join(__dirname, './dist/'), filename: 'js/bundle.js'
+    }, plugins: [htmlPlugin, new CleanWebpackPlugin()], devServer: {
         open: true, port: 80, host: '127.0.0.1'
     }, module: {
         rules: [{
@@ -15,7 +16,7 @@ module.exports = {
         }, {
             test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']
         }, {
-            test: /\.jpeg|jpg|gif|png$/, use: 'url-loader?limit=1024'
+            test: /\.jpeg|jpg|gif|png$/, use: 'url-loader?limit=1024&outputPath=images'
         }, {
             test: /\.js/, use: 'babel-loader', exclude: /node_modules/
         }]
